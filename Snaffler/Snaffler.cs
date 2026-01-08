873a7accffcabdaedf85147316c366d309529a8a
swinowz <97311975+swinowz@users.noreply.github.com>
2026-01-08 10:34:40 +0100

    removed out.txt redirect since it's not useful anymore (used for debug)

--- Snaffler/Snaffler.cs
+++ Snaffler/Snaffler.cs
@@ -7,18 +7,9 @@ namespace ShareAuditor
     {
         public static void Main(string[] args)
         {
-            // Redirect console output to out.txt file
-            var fileStream = new FileStream("out.txt", FileMode.Create, FileAccess.Write, FileShare.Read);
-            var streamWriter = new StreamWriter(fileStream);
-            streamWriter.AutoFlush = true;
-            Console.SetOut(streamWriter);
-            
             AuditorRunner runner = new AuditorRunner();
             runner.Run(args);
             Console.WriteLine("ShareAuditor scan complete.");
-            
-            streamWriter.Close();
-            fileStream.Close();
         }
     }
 }
